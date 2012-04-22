237c5cefb7de242c461487df370a4027b7cc6643
bertrand simonnet <iwky911@gmail.com>
2012-04-22 13:48:40 +0200

    added wrapper around kdnode

--- kdtree.py
+++ kdtree.py
@@ -1,39 +1,19 @@
-from plancomparator import *
+from kdtree import *
+from vector import *
 
-class kdNode:
+class kdTree:
     
-    def __init__(self, v, comp=None):
-        self.value=v
-        self.comparator=comp
-        self.isLeaf= True
-            
-    def addElt(self, elt):
-        if self.isLeaf:
-            moy = elt.mean(self.value)
-            self.comparator = PlanComparator(moy, elt)
-            left = kdNode(self.value)
-            right = kdNode(elt)
-            self.value=moy
-            self.setChildren(left, right)
-        else:
-            #~ print "inserting in node: ",self.getChildren(elt).value.values
-            self.getChildren(elt).addElt(elt)
-        
-    def getNearest(self, elt):
-        if self.isLeaf:
-            return (self.value, self.value.dist(elt))
-        else:
-            (val, cost) = self.getChildren(elt).getNearest(elt)
-            if self.comparator.distToPlan(elt) < cost:
-                nexttree = self.children[1] if self.getChildren(elt)==self.children[0] else self.children[0]
-                (val2, cost2) = nexttree.getNearest(elt)
-                if cost2 <cost:
-                    (val,cost) = (val2, cost2)
-            return (val, cost)
-            
-    def getChildren(self, elt):
-        return self.children[1] if self.comparator.isAfter(elt) else self.children[0]
+    def __init__(self):
+        self.root = None
     
-    def setChildren(self, left, right):
-        self.children= (left, right)
-        self.isLeaf = False
+    def add(self, e):
+        if self.root = None:
+            self.root = knNode(e)
+        else:
+            self.root.addElt(e)
+    
+    def search(self, e):
+        if self.root==None:
+            return None
+        (val, cost) = self.root.getNearest(e)
+        return val
